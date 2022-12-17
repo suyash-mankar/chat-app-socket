423d3d7e5a87e557e68e4e3a80f37c896bcd68da
Suyash Mankar <suyashmankar@gmail.com>
2022-12-18 04:39:02 +0530

    setup add user

--- index.js
+++ index.js
@@ -4,7 +4,21 @@ const io = require("socket.io")(server, {
     origin: "http://localhost:3000",
   },
 });
+
+let users = [];
+
+const addUser = (userData, clientId) => {
+  // if the userData doesn't exist in users array
+  !users.some((user) => user.sub === userData.sub) &&
+    users.push({ ...userData, clientId });
+};
+
 io.on("connection", (client) => {
   console.log("user connected");
+
+  client.on("addUsers", (userData) => {
+    addUser(userData, client.id);
+  });
 });
+
 server.listen(9000);
